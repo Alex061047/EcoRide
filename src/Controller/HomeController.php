caba4ecfe5d12793fa60703a3867d4a4c6a2f906
Alex N <alexnoble47@gmail.com>
2025-03-07 20:29:48 +0100

    Formulaire accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,37 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use App\Document\Itineraire;
+use App\Form\ItineraireType;
+
 
 final class HomeController extends AbstractController
 {
-    /**
- * Page d'accueil
- *
- * @return Response
- */
-
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, DocumentManager $dm): Response
     {
+        // Créer un nouvel itinéraire
+        $itineraire = new Itineraire();
+        $form = $this->createForm(ItineraireType::class, $itineraire);
+
+        // Gérer la requête du formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dm->persist($itineraire);
+            $dm->flush();
+
+            // Redirection après soumission
+            return $this->redirectToRoute('app_home'); 
+        }
+
+        // Rendre la vue avec le formulaire
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
-
-    #[Route('/create-itineraire', name: 'create_itineraire')]
-public function createItineraire(DocumentManager $dm): Response
-{
-    $itineraire = new Itineraire();
-    $itineraire->setDepart('Paris')
-               ->setArrive('Lyon')
-               ->setJour(new \DateTime('2025-03-10'));
-
-    $dm->persist($itineraire);
-    $dm->flush();
-
-    return new Response('Itinéraire créé avec ID : ' . $itineraire->getId());
-}
 }
